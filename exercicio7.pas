3788f07dd21ca8241358a31d12fecb2d722881ad
Edeno Scherer <edenoscherer@gmail.com>
2020-05-23 22:03:38 -0300

    Exercício 7 OK

--- exercicio7.pas
+++ exercicio7.pas
@@ -1,7 +1,24 @@
 program exercicio7;
 var
-    vetor1: ARRAY [4,5] OF integer;
-    count: integer;
+    vetor1: ARRAY[1..4,1..5] OF integer;
+    somaColunas: ARRAY[1..5] of integer = (0,0,0,0,0);
+    i, y, num: integer;
 begin
-    count := 0;
+    for i := 1 to 4 do
+    begin
+        for y := 1 to 5 do
+        begin
+            write('Informe um número inteiro para a posição ',i,'-',y,': ');
+            readln(num);
+            vetor1[i,y] := num;
+            somaColunas[y] := somaColunas[y]+num; 
+        end;
+    end;
+    num := 0;
+    for i := 1 to 5 do
+    begin
+        num := num + somaColunas[i];
+        writeln('A soma da coluna ', i, ' é: ', somaColunas[i]);
+    end;
+    writeln('A soma de todas as colunas é: ', num);
 end.
